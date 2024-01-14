491cb31689f84197b412bf1adec7ca82a13404f7
FatSassin <hubert@talwik.com>
2024-01-14 12:34:24 +0100

    Demon Lemon A Pair of Large Automobiles

--- src/main/java/net/fatsassin/depthcraft/datagen/ModItemTagGenerator.java
+++ src/main/java/net/fatsassin/depthcraft/datagen/ModItemTagGenerator.java
@@ -1,9 +1,11 @@
 package net.fatsassin.depthcraft.datagen;
 
 import net.fatsassin.depthcraft.Depthcraft;
+import net.fatsassin.depthcraft.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,11 @@ public class ModItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+        this.tag(ItemTags.MUSIC_DISCS)
+                .add(ModItems.TWO_TRUCKS_MUSIC_DISC.get());
+
+        this.tag(ItemTags.CREEPER_DROP_MUSIC_DISCS)
+                .add(ModItems.TWO_TRUCKS_MUSIC_DISC.get());
 
     }
 }
