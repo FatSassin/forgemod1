b37f03c99547ff66c3aff2e2ba611c8e65c5cf4d
FatSassin <hubert@talwik.com>
2024-01-15 17:39:39 +0100

    e

--- src/main/java/net/fatsassin/depthcraft/block/ModBlocks.java
+++ src/main/java/net/fatsassin/depthcraft/block/ModBlocks.java
@@ -7,6 +7,8 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,13 +22,17 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Depthcraft.MOD_ID);
 
-    public static final  RegistryObject<Block> ROCK_BLOCK = registerBlock("rock_block",
+    public static final RegistryObject<Block> ROCK_BLOCK = registerBlock("rock_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)));
 
-    public static final  RegistryObject<Block> DEATH_BLOCK = registerBlock("death_block",
+    public static final RegistryObject<Block> DEATH_BLOCK = registerBlock("death_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_BLOCK)));
-    public static final  RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
             () -> new SoundBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)));
+
+    //stair blocks and stuff
+    public static final RegistryObject<Block> ROCK_SLAB = registerBlock("rock_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
